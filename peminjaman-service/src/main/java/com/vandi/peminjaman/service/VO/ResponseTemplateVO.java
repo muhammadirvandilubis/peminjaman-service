63cc21bee48238fa2061fc419e9edb0cc19ab391
Muhammad Irvandi Lubis <113462342+muhammadirvandilubis@users.noreply.github.com>
2022-11-22 10:48:46 +0700

    Add files via upload

---
+++ peminjaman-service/src/main/java/com/vandi/peminjaman/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vandi.peminjaman.service.VO;
+
+import com.vandi.peminjaman.service.entity.Peminjaman;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author ASUS
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    private Peminjaman peminjaman;
+    private Anggota anggota;
+}
